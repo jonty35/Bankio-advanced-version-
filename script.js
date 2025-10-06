3b76210def6e0b317ceb576483340ad746219c8f
jonty35 <jontymeshramk@gmail.com>
2025-10-06 19:03:18 +0530

    initial commit

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const scrollbtn = document.querySelector(" .btn--scroll-to");
 const blurimg = document.querySelectorAll(".lazy-img");
+const toTop = document.querySelector(".totop");
+const navLinks = document.querySelector(".nav__links");
+const navLink = document.querySelector(".nav__link");
+const tabs = document.querySelector(".operations__tab-container");
+const opContent = document.querySelectorAll(".operations__content");
+const opTab = document.querySelectorAll(".operations__tab");
+
 
 
 
@@ -29,23 +36,57 @@ btnClosemodal.addEventListener("click", closeModal);
 
 
 scrollbtn.addEventListener("click", function()
-{
-    
+{ 
     const section1 = document.querySelector("#section--1");
 
     section1.scrollIntoView({behavior : "smooth"});
 });
 
-blurimg.forEach(img => img.addEventListener("mouseover", function(e){
+
+blurimg.forEach(img => img.addEventListener("mouseover", function(e)
+{
 
     e.currentTarget.style.filter = "blur(0px)";
+    const i = e.currentTarget.getAttribute("data-src");
+    e.currentTarget.src = i;
 
 }));
 
-const toTop = document.querySelector(".totop");
 
-toTop.addEventListener("click", function(){
+toTop.addEventListener("click", function()
+{
     const header = document.querySelector(".header");
 
     header.scrollIntoView({behavior : "smooth"});
+})
+
+
+navLinks.addEventListener("click", function(e){
+
+    e.preventDefault();
+
+    const id = e.target.getAttribute("href");
+   
+    if(e.target.classList.contains("nav__link"))
+    {
+        document.querySelector(id).scrollIntoView({behavior : "smooth"});
+    }
+
+});
+
+
+tabs.addEventListener("click", function(e){
+
+    const clicked = e.target.closest(".operations__tab");
+
+    if(!clicked) return ;
+
+    opTab.forEach(tab => tab.classList.remove("operations__tab--active"));
+
+    clicked.classList.add("operations__tab--active");
+
+    opContent.forEach(con => con.classList.remove("operations__content--active"));
+
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add("operations__content--active");
+ 
 })
